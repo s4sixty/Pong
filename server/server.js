76d47c8bd5e742c433c0b57e18220d3852111483
Samir <samirq2@outlook.com>
2020-04-23 19:04:21 +0200

    react front

--- server/server.js
+++ server/server.js
@@ -1,10 +1,27 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+var http = require('http').createServer(app);
+var cors = require('cors');
+// Chargement de socket.io
+var io = require('socket.io')(http);
+io.set('origins', '*:*');
 
+// Allow cors
+app.use(cors());
+// Autoriser des requetes de plusieurs domaines
+
+// Routes
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.listen(3000, function () {
+var server = app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
+
+// Quand un client se connecte, on le note dans la console
+io.on('connection', (socket) => {
+    res.writeHead(200, {
+        'Access-Control-Allow-Origin' : '*'
+    });
+});
